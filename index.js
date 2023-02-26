1e55dcdcc53bde5709757ee5f4fe15ddafc18374
mac <mac@macs-MacBook-Air.local>
2023-02-26 10:34:49 +0700

    final

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Configuration, OpenAIApi } = require("openai");
+const TelegramBot = require('node-telegram-bot-api');
+
+// API openAI key
+const config = new Configuration({
+    apiKey: "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"
+})
+
+// API telegram key
+const bot = new TelegramBot('6152642215:AAHHqY2zIcHKioN8XMU_bkgcmYUg2x10u8I', {
+    polling: true
+});
+
+bot.onText(/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Hello human!');
+});
+
+const openAI = new OpenAIApi(config)
+bot.on('message', async (msg) => {
+    try {
+        const response = await openAI.createCompletion({
+            model: "text-davinci-003",
+            prompt: msg.text,
+            max_tokens: 2048,
+            temperature: 1,
+        })
+
+        const { choices } = response.data;
+        const { text } = choices[0];
+
+        console.log(response.data); //untuk menampilkan data API
+
+        bot.sendMessage(msg.chat.id, text);
+    } catch (error) {
+        console.error(error);
+        bot.sendMessage(msg.chat.id, 'Sorry, something went wrong.');
+    }
+});
